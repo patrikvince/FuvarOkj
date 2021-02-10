a025b9b09c3fbfbc8b589c2315380493f44f7dac
Vince Patrik <patrikvince@gmail.com>
2021-02-10 12:17:09 +0100

    3. feladat

---
+++ Fuvar/Fuvar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuvar
+{
+    class Fuvar
+    {
+        private int id, idotartam;
+        private string indulas, fizm;
+        private double megtett, vitel, borr; 
+
+        public Fuvar(string sor)
+        {
+            string[] s = sor.Split(';');
+            this.id = int.Parse(s[0]);
+            this.indulas = s[1];
+            this.idotartam = int.Parse(s[2]);
+            this.megtett = double.Parse(s[3]);
+            this.vitel = double.Parse(s[4]);
+            this.borr = double.Parse(s[5]);
+            this.fizm = (s[6]);
+        }
+    }
+}
---
+++ Fuvar/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuvar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sorok = File.ReadAllLines("fuvar.csv");
+            List<Fuvar> fuvarok = new List<Fuvar>();
+
+            foreach (String sor in sorok.Skip(1))
+            {
+                fuvarok.Add(new Fuvar(sor));
+            }
+
+            Console.WriteLine($"3. feladat: {fuvarok.Count} fuvar");
+            
+
+            Console.ReadLine();
+        }
+    }
+}
