131fdb9346ca73c62b7505d3e0dcb4f1d886aeed
Vince Patrik <patrikvince@gmail.com>
2021-02-10 13:04:47 +0100

    6. feladatig

--- Fuvar/Fuvar.cs
+++ Fuvar/Fuvar.cs
@@ -8,20 +8,25 @@ namespace Fuvar
 {
     class Fuvar
     {
-        private int id, idotartam;
-        private string indulas, fizm;
-        private double megtett, vitel, borr; 
 
         public Fuvar(string sor)
         {
             string[] s = sor.Split(';');
-            this.id = int.Parse(s[0]);
-            this.indulas = s[1];
-            this.idotartam = int.Parse(s[2]);
-            this.megtett = double.Parse(s[3]);
-            this.vitel = double.Parse(s[4]);
-            this.borr = double.Parse(s[5]);
-            this.fizm = (s[6]);
+            ID = int.Parse(s[0]);
+            Indulas = s[1];
+            Idotartam = int.Parse(s[2]);
+            Megtett = double.Parse(s[3]);
+            Vitel = double.Parse(s[4]);
+            Borr = double.Parse(s[5]);
+            Fizm = (s[6]);
         }
+
+        public int ID { get;  }
+        public string Indulas { get;  }
+        public int Idotartam { get;  }
+        public double Megtett { get;  }
+        public double Vitel { get;  }
+        public string Fizm { get; }
+        public double Borr { get; }
     }
 }
--- Fuvar/Program.cs
+++ Fuvar/Program.cs
@@ -20,7 +20,50 @@ namespace Fuvar
             }
 
             Console.WriteLine($"3. feladat: {fuvarok.Count} fuvar");
+
+            int db = 0;
+            double fiz = 0;
+            foreach (Fuvar fuvar in fuvarok)
+            {
+                if (fuvar.ID == 6185)
+                {
+                    db++;
+                    fiz += fuvar.Vitel;
+                    fiz += fuvar.Borr;
+                }
+            }
+
+            Console.WriteLine($"4. feladat: {db} fuvar alatt: {fiz:N2} $");
+
+            Dictionary<string, int> fizetesiModok = new Dictionary<string, int>();
             
+            foreach (Fuvar fuvar1 in fuvarok)
+            {
+                if (fizetesiModok.ContainsKey(fuvar1.Fizm))
+                {
+                    fizetesiModok[fuvar1.Fizm]++;
+                }
+                else
+                {
+                    fizetesiModok.Add(fuvar1.Fizm, 1);
+                } 
+            }
+
+            Console.WriteLine("5. feladat:");
+
+            foreach (KeyValuePair<string, int> item in fizetesiModok)
+            {
+                Console.WriteLine($"\t{item.Key}: {item.Value} fuvar");
+            }
+
+            double km = 0;
+            foreach (Fuvar fuvar2 in fuvarok)
+            {
+                km += fuvar2.Megtett * 1.6;
+            }
+
+            Console.WriteLine($"6. feladat: {km}km");
+
 
             Console.ReadLine();
         }
